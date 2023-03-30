33fed149bcc11eac63ffb05cfa8d9ae3d09e5f1d
NuminousBlue <jdiehl2236@gmail.com>
2023-03-30 08:21:34 -0500

    parse input args!

--- cli.py
+++ cli.py
@@ -1,5 +1,34 @@
 import sys
 
+
 def get_input():
-    filepath = sys.argv[1]
+    parser = InputParser()
+    parser.print()
+    filepath = parser[1]
+    if "-s" in parser.args():
+        parser.set_manual_size()
     return filepath
+
+
+class InputParser:
+    def __init__(self):
+        self.inputs = sys.argv
+        self.size = None
+
+    def set_manual_size(self):
+        try:
+            arg_idx = self.inputs.index("-s")
+            value = self.inputs[arg_idx+1]
+            dims = value.split('x')
+
+            self.size = [dims[0], dims[2]]
+            print(f"Size argument: {self.inputs[arg_idx]}")
+            print(f"Size arg value: {value}")
+        except IndexError:
+            print("")
+
+    def print(self):
+        print(f"Command line arguments: {self.inputs}")
+
+    def args(self):
+        return self.inputs
--- style.py
+++ style.py
@@ -2,7 +2,6 @@ from colorama import Fore, Back, Style
 
 
 def alert(txt: str):
-
-    print(Fore.CYAN + "\n=============\n")
+    print(Fore.CYAN + f"\n{ '=' * 66}\n")
     print(Fore.CYAN + "!" + Fore.MAGENTA + txt + Fore.CYAN + "!" + "\n")
-    print(Fore.CYAN, "============")
+    print(Fore.CYAN, "="*66)
